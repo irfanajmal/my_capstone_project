99c2427bae9f5a1b5d0cf46a31ec4c43aac77ff2
Irfan <irfanajmal@gmail.com>
2020-04-01 15:43:54 -0600

    Demo

--- hello/db.py
+++ hello/db.py
@@ -3,7 +3,7 @@ import psycopg2
 
 class myDB():
 	def connect():
-		db_url = subprocess.check_output(['heroku', 'config:get', 'HEROKU_POSTGRESQL_CYAN_URL'])
+		db_url = os.environ['HEROKU_POSTGRESQL_CYAN_URL']
 		MY_DATABASE_URL = db_url[0:len(db_url)-1]
 		conn = psycopg2.connect(MY_DATABASE_URL, sslmode='require')
 		return conn
