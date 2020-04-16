316e74d416ee5e68c5b85c9a1889f1b947d49c89
irfan <irfanajmal@gmail.com>
2020-04-16 03:51:27 -0600

    trying to fix static files issue

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -119,6 +119,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dbdump'),
+)
+
 DBDUMP_ROOT = os.path.join(BASE_DIR, 'dbdump')
 DBDUMP_URL = '/dbdump/'
 
